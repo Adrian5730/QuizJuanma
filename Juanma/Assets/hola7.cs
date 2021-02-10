4bfda8e2610905725ad9fe2e8e9b8a67bf7be53b
Adrian Olave <43561326+Adrian5730@users.noreply.github.com>
2021-02-10 09:58:02 -0300

    Termino Ejercicio 7

--- Juanma/Assets/hola7.cs
+++ Juanma/Assets/hola7.cs
@@ -4,78 +4,161 @@ using UnityEngine;
 
 public class hola7 : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
+
     void Start()
     {
-        /*En un desafío del Super Smash Bros, cuatros personajes se enfrentan (Mario, Kong,
-        Pikachu, Link), 
-        
-        el desafío consta de 10 turnos,
 
-        en cada turno se deben recolectar esferas, cada personaje puede recolectar entre 10 y 30 esferas.
-        
-        Los puntajes de cada  turno serán guardados en un array por personaje.
-        Al finalizar los 10 turnos indicar
-
-        a. Cuantas esferas en total recolectaron todos los personajes en cada
-        turno:
-        Por ejemplo: Turno 1 se recolectaron 59 esferas, Turno 2 se recolectaron 65
-        esferas, etc.
-        
-        b. Indicar quién fue el ganador de cada ronda. (en caso de que haya un empate nadie gana la ronda). 
-        
-        c. Indique cuál de los 4 jugadores ganó la competencia, sabiendo que el que haya ganado más rondas es el ganador. 
-        
-        d. Los resultados se deben indicar después  del juego, y no durante la batalla.
-        */
-        
-
-
-        int[] esferasMario = new int [10];
+        //Primeros Turnos
+        int[] esferasMario = new int[10];
         int[] esferasKong = new int[10];
         int[] esferasPikachu = new int[10];
         int[] esferasLink = new int[10];
-        var totalEsferas = 0;
+        int totalpuntajes = 0;
         int contadorTurno = 1;
-
-        for (int i = 0; i < 1; i++)
+        int[] rondasGanadas = new int[4] { 0, 0, 0, 0 };
+        int indicadorGanador = 0;
+        string ganador = "";
+        for (int a = 0; a < 10; a++)
         {
-            esferasMario[i] = Random.Range(10, 31);
-            esferasKong[i] = Random.Range(10, 31);
-            esferasPikachu[i] = Random.Range(10, 31);
-            esferasLink[i] = Random.Range(10, 31);
-            int[] puntajeRonda = new int[4] { esferasMario[i], esferasKong[i], esferasPikachu[i], esferasLink[i] };
-
-            var rondasGanadasMario = 0;
-            var rondasGanadasKong = 0;
-            var rondasGandasPikachu = 0;
-            var rondasGanadsLink = 0;
-            var GanadorDeRonda = "";
-            var mayor = 0;
-            for (int a = 0; a < puntajeRonda[i]; a++)
-            {
-                if (puntajeRonda[a] > mayor)
-                {
-                    mayor = puntajeRonda[a];
-                    Debug.Log("El puntaje mayor de la ronda numero " + a + " fue " + mayor);
-                }
-            }
-           
-
-            // a. Cuantas esferas en total recolectaron todos los personajes en cada turno
-            totalEsferas = esferasMario[i] + esferasKong[i] + esferasPikachu[i] + esferasLink[i];
-            Debug.Log("Turno " + contadorTurno + " se recolectaron " + totalEsferas);
-            contadorTurno++;
-
-            // b. Indicar quién fue el ganador de cada ronda. (en caso de que haya un empate nadie gana la ronda). 
-
-           
-
+            esferasMario[a] = Random.Range(10, 31);
+            esferasKong[a] = Random.Range(10, 31);
+            esferasPikachu[a] = Random.Range(10, 31);
+            esferasLink[a] = Random.Range(10, 31);
         }
 
 
+        for (int i = 0; i < 10; i++) { 
+           
+            int[] puntajesRonda = new int[4] { esferasMario[i], esferasKong[i], esferasPikachu[i], esferasLink[i] };
+
+            totalpuntajes = esferasMario[i] + esferasKong[i] + esferasPikachu[i] + esferasLink[i];
+
+            // a. Cuantas esferas en total recolectaron todos los personajes en cada turno
+ 
+            Debug.Log("Turno " + contadorTurno + " se recolectaron " + totalpuntajes);
+            contadorTurno++;
+
+
+            // Se pregunta el valor maximo de puntos
+            int valorMax = 0;
+            int indicardorGanador = 0;
+            int[] otrosValores = new int [3];
+            string ganadorRonda = "";
+
+            for (int a = 0; a < puntajesRonda.Length; a++)
+            {
+
+                if (puntajesRonda[a] > valorMax)
+                {
+                    valorMax = puntajesRonda[a];
+                    indicardorGanador = a;
+                    
+                }
+
+            }
+
+
+            // Se le saca al array de los puntajes para comparar si igualan el puntaje
+            switch (indicardorGanador)
+            {
+                case 0:
+                   otrosValores = new int[3] { puntajesRonda[1], puntajesRonda[2], puntajesRonda[3] };
+                    break;
+
+                case 1:
+                    otrosValores = new int[3] { puntajesRonda[0], puntajesRonda[2], puntajesRonda[3] };
+                    break;
+
+                case 2:
+                    otrosValores = new int[3] { puntajesRonda[0], puntajesRonda[1], puntajesRonda[3] };
+                    break;
+
+                case 3:
+                    otrosValores = new int[3] { puntajesRonda[0], puntajesRonda[1], puntajesRonda[2] };
+                    break;
+            }
+
+            for (int x = 0; x < otrosValores.Length; x++)
+            {
+
+                if (otrosValores[x] == valorMax)
+                {
+                    indicardorGanador = 4;
+                }
+            };
+
+
+            // Se elije al ganador de la ronda
+            
+            switch (indicardorGanador)
+            {
+                case 0:
+                    ganadorRonda = "Mario";
+                    rondasGanadas[0 ]++;
+                    break;
+
+                case 1:
+                    ganadorRonda = "Kong";
+                    rondasGanadas[1]++;
+                    break;
+
+                case 2:
+                    ganadorRonda = "Pikachu";
+                    rondasGanadas[2]++;
+                    break;
+
+                case 3:
+                    ganadorRonda = "Link";
+                    rondasGanadas[3]++;
+                    break;
+
+                case 4:
+                    ganadorRonda = "nadie empate";
+                    break;
+            }
+            // b.Indicar quién fue el ganador de cada ronda. (en caso de que haya un empate nadie gana la ronda)
+            Debug.Log("El ganador de la ronda es " + ganadorRonda);
+
+           
+        }
+
+        
+
+       //Se recorre las rondas ganadas para saber quien es el ganador
+
+        for (int i = 0; i < rondasGanadas.Length; i++)
+        {
+            if (rondasGanadas[i] > indicadorGanador)
+            {
+                indicadorGanador = i;
+            }
+        }
+
+        switch (indicadorGanador)
+        {
+            case 0:
+                ganador = "Mario";
+                break;
+
+            case 1:
+                ganador = "Kong";
+                break;
+
+            case 2:
+                ganador = "Pikachu";
+                break;
+
+            case 3:
+                ganador = "Link";
+                break;
+        }
+
+        //c. Indique cuál de los 4 jugadores ganó la competencia, sabiendo que el que haya ganado más rondas es el ganador.
+
+        Debug.Log("El ganador del juego es " + ganador);
+
 
     }
-
 
 }
